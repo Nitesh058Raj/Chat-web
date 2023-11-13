366d6dcae26c71ef024d68d3fed67546bc7aab02
Nitesh <05niteshraj@gmail.com>
2023-11-13 12:03:55 +0530

    Moved files;

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
-  declarations: [LoginComponent],
-  imports: [CommonModule, ReactiveFormsModule],
-  exports: [LoginComponent],
+  declarations: [],
+  imports: [CommonModule],
+  exports: [],
 })
 export class SharedModule {}
