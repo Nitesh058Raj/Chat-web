b1c4a372a284c53d96c906fcbd5515dfb709220c
Nitesh <05niteshraj@gmail.com>
2023-11-13 12:02:52 +0530

    Added routing and moved files;

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,6 +15,5 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    
   });
 });
