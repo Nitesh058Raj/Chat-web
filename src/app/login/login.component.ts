8574435ad6a462c6c48ae1cdaca7a746681a501f
Nitesh <05niteshraj@gmail.com>
2024-01-21 18:49:15 +0530

    Moved files;

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from 'src/app/shared/interfaces/shared.model';
+import { UserService } from 'src/app/shared/services/user.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  @ViewChild('form') form: any;
+
+  userName!: FormControl;
+  loginForm!: FormGroup;
+
+  userDetails!: User;
+
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.userName = new FormControl('');
+
+    this.loginForm = this.fb.group({
+      userName: this.userName,
+    });
+  }
+
+  login() {
+    if (this.loginForm.invalid || this.loginForm.value.userName === '') {
+      return;
+    }
+
+    this.userDetails = {
+      name: this.loginForm.value.userName,
+    };
+
+    this.userService.setUserDetails(this.userDetails);
+    this.router.navigate(['/chat-window']);
+  }
+}
